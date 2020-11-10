196cdb5c41b9c9be10adb58a0e9c5041abcd8f94
boonepeter <boonepeterg@gmail.com>
2020-11-10 00:50:04 -0500

    update description

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from .routers import problems
 app = FastAPI(
     title="Franklin API",
     description="""My solutions to the [Rosalind](http://rosalind.info/) problem set, grouped by tag (some endpoints have multiple tags). 
-    View the [source code here](https://github.com/boonepeter/franklin-api)."""
+    View the [source code here](https://github.com/boonepeter/franklin-api) (Currently private to not distribute solutions)."""
 )
 
 app.include_router(problems.router, prefix="/problems")
